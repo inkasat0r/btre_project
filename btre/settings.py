6e4b5c7a47331cd4afe22ffcc2ddb4127617808d
inkasat0r <inkasator@gmail.com>
2019-12-14 00:26:24 +0100

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -151,3 +151,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER='poligon.racun@gmail.com'
 EMAIL_HOST_PASSWORD='pr2forum'
 EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
